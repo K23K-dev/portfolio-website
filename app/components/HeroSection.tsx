5f577287b34ff416543b593438b350b97e441a9c
Kevin Kou <kevinkou0930@gmail.com>
2025-07-25 00:39:02 +0800

    Installs framer motion

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,9 @@
 const HeroSection = () => {
   return (
-    <div className="bg-black w-full min-h-screen">
-      <h1 className="text-white">Hi, I am Kevin Kou</h1>
+    <div className="bg-black w-full min-h-screen flex justify-center items-center">
+      <h1 className="text-white font-bold text-3xl">
+        Hi, I am Kevin Kou
+      </h1>
     </div>
   );
 };
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,6 +1,6 @@
 const Projects = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black w-full min-h-screen">
       <h2 className="text-white">My Projects</h2>
     </div>
   );
